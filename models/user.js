c542cb6d1d137bca80012276b19e854b7c7f4c87
Aakriti Yadav <aakriti.yadav@bigsteptech.com>
2021-06-05 20:49:04 +0530

    merging

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "This Field is Required"]
+    },
+    amount: {
+        type: Number
+    },
+    address: {
+        type: String
+    },
+    gift: String,
+    mobile: {
+        type: Number
+    }
+});
+
+
+const userTable = mongoose.model('userTable', userSchema);
+module.exports = userTable;
